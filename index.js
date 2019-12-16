6d81339ce4b09225e3ea8492325ecaf9f3046b78
dschawel <david.schawel@gmail.com>
2019-12-16 15:29:42 -0800

    added home page

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let express = require('express')
+let layouts = require('express-ejs-layouts')
 
 let app = express()
 
 app.set('view engine', 'ejs')
+app.use(layouts)
 
 app.get('/', (req, res) => {
-    res.send('Hi')
+    res.render('home')
 })
 
 app.listen(process.env.PORT || 3000)
